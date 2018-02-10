41283dee13aebc57d249c8b4445542172db9f81a
developer <developer@cansat-workstation>
2018-02-10 21:25:28 +0300

    Начата работа над единой абстракцией для единообразного доступа к внешним устройствам

---
+++ research/omnubus-reborn/system-usr/granum_common.h
@@ -0,0 +1,17 @@
+/*
+ * granum_common.h
+ *
+ *  Created on: 10 февр. 2018 г.
+ *      Author: developer
+ */
+
+#ifndef GRANUM_COMMON_H_
+#define GRANUM_COMMON_H_
+
+typedef enum {
+
+} gr_error_t
+
+
+
+#endif /* GRANUM_COMMON_H_ */
