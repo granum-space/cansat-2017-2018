d224162f64bf567aa74dc145aad104b1f6cee602
developer <developer@cansat-workstation>
2018-02-21 21:58:35 +0300

    Devctl почи закончен (осталось протестировать и добавить работу с потоковыми интерфейсами)

--- research/omnubus-reborn/system-usr/granum_common.h
+++ research/omnubus-reborn/system-usr/granum_common.h
@@ -8,12 +8,68 @@
 #ifndef GRANUM_COMMON_H_
 #define GRANUM_COMMON_H_
 
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_i2c.h"
+
+typedef struct {
+	enum {
+		GR_IF_PARAM_SPEED_SLOW,
+		GR_IF_PARAM_SPEED_FAST,
+	} speed;
+
+	uint32_t timeout;
+
+
+
+} gr_interface_params_t;
+
+typedef struct {
+	enum {
+		GR_IF_TYPE_I2C,
+		GR_IF_TYPE_SPI,
+		GR_IF_TYPE_STREAM,
+	} type;
+
+	void * descriptor;
+
+	bool inited;
+
+	gr_interface_params_t params;
+
+	xSemaphoreHandle mutex;
+} gr_interface_t;
+
+#define GR_ERROR_FROMHAL(STATUS) STATUS
+
 typedef enum {
 	GR_ERROR_OK,
 	GR_ERROR_WRONGARG,
 } gr_error_t;
 
-int gr_if_i2c_clockSpeeds[] = {100000, 100000, 100000};
+extern gr_interface_t GR_IF_I2C_1;
+#define GR_INTERFACE_I2C_1_SPEED_SLOW 100000
+#define GR_INTERFACE_I2C_1_SPEED_FAST 400000
+
+extern gr_interface_t GR_IF_I2C_2;
+#define GR_INTERFACE_I2C_2_SPEED_SLOW 100000
+#define GR_INTERFACE_I2C_2_SPEED_FAST 400000
+
+extern gr_interface_t GR_IF_I2C_3;
+#define GR_INTERFACE_I2C_3_SPEED_SLOW 100000
+#define GR_INTERFACE_I2C_3_SPEED_FAST 400000
+
+extern gr_interface_t GR_IF_SPI_1;
+#define GR_INTERFACE_SPI_1_PRESC_SLOW SPI_BAUDRATEPRESCALER_128
+#define GR_INTERFACE_SPI_1_PRESC_FAST SPI_BAUDRATEPRESCALER_4
+
+extern gr_interface_t GR_IF_SPI_2;
+#define GR_INTERFACE_SPI_2_PRESC_SLOW SPI_BAUDRATEPRESCALER_128
+#define GR_INTERFACE_SPI_2_PRESC_FAST SPI_BAUDRATEPRESCALER_4
+
+extern gr_interface_t GR_IF_SPI_3;
+#define GR_INTERFACE_SPI_3_PRESC_SLOW SPI_BAUDRATEPRESCALER_128
+#define GR_INTERFACE_SPI_3_PRESC_FAST SPI_BAUDRATEPRESCALER_4
+
 
 
 #endif /* GRANUM_COMMON_H_ */
--- research/omnubus-reborn/system-usr/src/devctl.c
+++ research/omnubus-reborn/system-usr/src/devctl.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdbool.h>
 
 #include "stm32f4xx_hal.h"
@@ -6,70 +7,147 @@
 #include "stm32f4xx_hal_uart.h"
 #include "stm32f4xx_hal_gpio.h"
 
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+#include "granum_common.h"
+
 #include "devctl.h"
 
 
+#define ERRORCHECK(EXPR) retval = EXPR; goto end;
 
-struct gr_interface_struct {
-	enum {
-		GR_IF_TYPE_I2C,
-		GR_IF_TYPE_SPI,
-		GR_IF_TYPE_STREAM,
-	} type;
+static inline gr_error_t _chparams_i2c(gr_interface_t * iface, gr_interface_params_t * params) {
+	I2C_HandleTypeDef * descr = iface->descriptor;
 
-	void * descriptor;
+	uint32_t newSPEED = 0;
 
-	bool inited;
+	switch (params->speed) {
+		case GR_IF_PARAM_SPEED_SLOW:
+			switch ( (size_t) descr->Instance ) {
+			case (size_t) SPI1:
+				newSPEED = GR_INTERFACE_I2C_1_SPEED_SLOW;
+				break;
 
-	gr_interface_params_t params;
-};
+			case (size_t) SPI2:
+				newSPEED = GR_INTERFACE_I2C_2_SPEED_SLOW;
+				break;
 
-I2C_HandleTypeDef i2c1 = {.Instance = I2C1};
-gr_interface_t GR_IF_I2C_1 = {.type = GR_IF_TYPE_I2C, .descriptor = &i2c1, .inited = false};
+			case (size_t) SPI3:
+				newSPEED = GR_INTERFACE_I2C_3_SPEED_SLOW;
+				break;
 
-I2C_HandleTypeDef i2c2 = {.Instance = I2C2};
-gr_interface_t GR_IF_I2C_2 = {.type = GR_IF_TYPE_I2C, .descriptor = &i2c2, .inited = false};
+			default:
+				return GR_ERROR_WRONGARG;
+			}
 
-I2C_HandleTypeDef i2c3 = {.Instance = I2C3};
-gr_interface_t GR_IF_I2C_3 = {.type = GR_IF_TYPE_I2C, .descriptor = &i2c3, .inited = false};
+			break;
 
+		case GR_IF_PARAM_SPEED_FAST:
+			switch ( (size_t) descr->Instance ) {
+			case (size_t) SPI1:
+				newSPEED = GR_INTERFACE_I2C_1_SPEED_FAST;
+				break;
 
-SPI_HandleTypeDef spi1 = {.Instance = SPI1};
-gr_interface_t GR_IF_SPI_1 = {.type = GR_IF_TYPE_SPI, .descriptor = &spi1, .inited = false};
+			case (size_t) SPI2:
+				newSPEED = GR_INTERFACE_I2C_2_SPEED_FAST;
+				break;
 
-SPI_HandleTypeDef spi2 = {.Instance = SPI2};
-gr_interface_t GR_IF_SPI_2 = {.type = GR_IF_TYPE_SPI, .descriptor = &spi2, .inited = false};
+			case (size_t) SPI3:
+				newSPEED = GR_INTERFACE_I2C_3_SPEED_FAST;
+				break;
 
-SPI_HandleTypeDef spi3 = {.Instance = SPI3};
-gr_interface_t GR_IF_SPI_3 = {.type = GR_IF_TYPE_SPI, .descriptor = &spi3, .inited = false};
+			default:
+				return GR_ERROR_WRONGARG;
+			}
 
+			break;
 
-static gr_error_t _init_spi(gr_interface_t * iface) {
-	if(!iface->inited) {
+		default:
+			return GR_ERROR_WRONGARG;
+	}
+
+	HAL_I2C_DeInit(descr); //TODO мб деинитить и не надо?
+
+	descr->Init.ClockSpeed = newSPEED;
+
+	HAL_I2C_Init(descr);
+
+	memcpy(&(iface->params), params, sizeof(gr_interface_params_t) );
+
+	return GR_ERROR_OK;
+}
+
+static inline gr_error_t _chparams_spi(gr_interface_t * iface, gr_interface_params_t * params) {
+	SPI_HandleTypeDef * descr = iface->descriptor;
+
+	uint32_t newPRESC = 0;
+
+	switch (params->speed) {
+		case GR_IF_PARAM_SPEED_SLOW:
+			switch ( (size_t) descr->Instance ) {
+			case (size_t) SPI1:
+				newPRESC = GR_INTERFACE_SPI_1_PRESC_SLOW;
+				break;
+
+			case (size_t) SPI2:
+				newPRESC = GR_INTERFACE_SPI_2_PRESC_SLOW;
+				break;
+
+			case (size_t) SPI3:
+				newPRESC = GR_INTERFACE_SPI_3_PRESC_SLOW;
+				break;
+
+			default:
+				return GR_ERROR_WRONGARG;
+			}
+
+			break;
+
+		case GR_IF_PARAM_SPEED_FAST:
+			switch ( (size_t) descr->Instance ) {
+			case (size_t) SPI1:
+				newPRESC = GR_INTERFACE_SPI_1_PRESC_FAST;
+				break;
+
+			case (size_t) SPI2:
+				newPRESC = GR_INTERFACE_SPI_2_PRESC_FAST;
+				break;
+
+			case (size_t) SPI3:
+				newPRESC = GR_INTERFACE_SPI_3_PRESC_FAST;
+				break;
+
+			default:
+				return GR_ERROR_WRONGARG;
+			}
+
+			break;
+
+		default:
+			return GR_ERROR_WRONGARG;
 
 	}
 
+	HAL_SPI_DeInit(descr); //TODO мб деинитить и не надо?
+
+	descr->Init.BaudRatePrescaler = newPRESC;
+
+	HAL_SPI_Init(descr);
+
+	memcpy(&(iface->params), params, sizeof(gr_interface_params_t) );
+
 	return GR_ERROR_OK;
 }
 
-static gr_error_t _chparams_spi(gr_interface_t * iface, gr_interface_params_t * params) {
-
-
-	return GR_ERROR_OK;
-}
-
-static gr_error_t _init_i2c(gr_interface_t * iface) {
-	return GR_ERROR_OK;
-}
-
-static gr_error_t _init_on_need(gr_interface_t * iface) {
+static inline gr_error_t _init_on_need(gr_interface_t * iface) {
 	if(!iface->inited) {
 		switch(iface->type) {
 		case GR_IF_TYPE_I2C:
-			return _init_i2c(iface);
+			return GR_ERROR_FROMHAL( HAL_I2C_Init(iface->descriptor) );
 
 		case GR_IF_TYPE_SPI:
-			return _init_spi(iface);
+			return GR_ERROR_FROMHAL( HAL_SPI_Init(iface->descriptor) );
 
 		case GR_IF_TYPE_STREAM:
 			//there should be stream ifaces handling
@@ -83,13 +161,17 @@ static gr_error_t _init_on_need(gr_interface_t * iface) {
 }
 
 gr_error_t gr_if_set_params(gr_interface_t * iface, gr_interface_params_t * params) {
+	xSemaphoreTake(iface->mutex, 0);
+
+	gr_error_t retval;
+
 	if( !memcmp( &(iface->params), params, sizeof(gr_interface_params_t) ) ) {
 		switch(iface->type) {
 		case GR_IF_TYPE_I2C:
-			return _chparams_i2c(iface, params);
+			ERRORCHECK( _chparams_i2c(iface, params) )
 
 		case GR_IF_TYPE_SPI:
-			return _chparams_spi(iface, params);
+			ERRORCHECK( _chparams_spi(iface, params) )
 
 		case GR_IF_TYPE_STREAM:
 			//there should be stream ifaces handling
@@ -97,7 +179,67 @@ gr_error_t gr_if_set_params(gr_interface_t * iface, gr_interface_params_t * para
 		default:
 			return GR_ERROR_WRONGARG;
 		}
+
 	}
 
+end:
+	xSemaphoreGive(iface->mutex);
 	return GR_ERROR_OK;
+}
+
+static inline _readreg_i2c(gr_interface_t * iface, uint8_t dev_addr, uint8_t reg_addr, void * buffer, size_t count) {
+	return GR_ERROR_FROMHAL( HAL_I2C_Mem_Read(iface->descriptor, dev_addr, reg_addr, I2C_MEMADD_SIZE_8BIT, buffer, count, iface->params.timeout) );
+}
+
+static inline _writereg_i2c(gr_interface_t * iface, uint8_t dev_addr, uint8_t reg_addr, void * buffer, size_t count) {
+	return GR_ERROR_FROMHAL( HAL_I2C_Mem_Write(iface->descriptor, dev_addr, reg_addr, I2C_MEMADD_SIZE_8BIT, buffer, count, iface->params.timeout) );
+}
+
+static inline _readreg_spi(gr_interface_t * iface, uint8_t dev_addr, uint8_t reg_addr, void * buffer, size_t count) {
+
+}
+
+gr_error_t gr_dev_readreg(gr_interface_t * iface, uint8_t dev_addr, uint8_t reg_addr, void * buffer, size_t count) {
+	xSemaphoreTake(iface->mutex, 0);
+	gr_error_t retval;
+
+	_init_on_need(iface);
+
+	switch( iface->type ) {
+	case GR_IF_TYPE_I2C:
+		ERRORCHECK( _readreg_i2c(iface, dev_addr, reg_addr, buffer, count) )
+
+	case GR_IF_TYPE_SPI:
+		ERRORCHECK( _readreg_spi(iface, dev_addr, reg_addr, buffer, count) )
+
+	default:
+		ERRORCHECK( GR_ERROR_WRONGARG )
+
+	}
+
+end:
+	xSemaphoreGive(iface->mutex);
+	return retval;
+}
+
+gr_error_t gr_dev_writereg(gr_interface_t * iface, uint8_t dev_addr, uint8_t reg_addr, void * buffer, size_t count) {
+	xSemaphoreTake(iface->mutex, 0);
+	gr_error_t retval;
+
+	_init_on_need(iface);
+
+	switch( iface->type ) {
+	case GR_IF_TYPE_I2C:
+		ERRORCHECK( _writereg_i2c(iface, dev_addr, reg_addr, buffer, count) )
+
+	case GR_IF_TYPE_SPI:
+		ERRORCHECK( _writereg_spi(iface, dev_addr, reg_addr, buffer, count) )
+
+	default:
+		ERRORCHECK( GR_ERROR_WRONGARG )
+	}
+
+end:
+	xSemaphoreGive(iface->mutex);
+	return retval;
 }
