9036bccfee26ea2c7c8bdaffddd7be38173c82bd
volospolina <volospolina148@gmail.com>
2018-03-25 18:11:45 +0300

    код для сервы

--- src/board/servo_ctl/Project/stm8_granum_servo_ctl/main.c
+++ src/board/servo_ctl/Project/stm8_granum_servo_ctl/main.c
@@ -31,29 +31,67 @@
 #include "stm8s_it.h"    // SDCC requires ISR declaration to be included here
 
 #include "stm8s_gpio.h"
+#include "stm8s_tim2.h"
 
 /* Private defines -----------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
-void main(void)
-{
-	int value = 0;
+void blink(void) {
 
-	/* Infinite loop */
-	GPIO_Init(GPIOC, GPIO_PIN_3, GPIO_MODE_OUT_OD_LOW_FAST);
+	uint8_t port = GPIO_ReadOutputData(GPIOC);
+	port ^= (1 << 3);
+	GPIO_Write(GPIOC, port);
+}
 
-	while (1)
-	{
-		GPIO_Write(GPIOC, (value & 0x01) << 3);
-		value = !value;
+void delay(long int cycles) {
 
-		for (long int i = 0; i < 60000; i++)
-		{
-			volatile int x = 0;
-		}
+	for (long int i = 0; i < cycles; i++) {
+
+		volatile int x = 13;
 	}
 }
+
+void rotate(int angle) {
+
+	uint16_t time = (angle / 180.0) * 2000 + 2000;
+	TIM2_SetCompare1(time);
+}
+
+void main(void) {
+	TIM2_TimeBaseInit(TIM2_PRESCALER_1, 40000);
+	TIM2_OC1Init(TIM2_OCMODE_PWM2, TIM2_OUTPUTSTATE_ENABLE, 4000, TIM2_OCPOLARITY_LOW);
+	TIM2_OC1PreloadConfig(ENABLE);
+
+
+	TIM2_Cmd(ENABLE);
+
+	GPIO_Init(GPIOC, GPIO_PIN_3, GPIO_MODE_OUT_OD_LOW_FAST);
+	//GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_HIGH_SLOW);
+
+	while(1) {
+
+		for(int i = 0; i < 3; i++) {
+
+			rotate(180);
+			delay(30000);
+			rotate(0);
+			delay(30000);
+
+		}
+
+		for(int i = 0; i < 3; i++) {
+
+					rotate(180);
+					delay(30000);
+					rotate(0);
+					delay(90000);
+
+		}
+
+	}
+}
+
 
 #ifdef USE_FULL_ASSERT
 
