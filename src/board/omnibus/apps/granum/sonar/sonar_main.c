84aa6ff1046d9e8086cd7e75d1503601b76aa0e4
ingacansat <ingacrane@gmail.com>
2018-03-31 19:15:11 +0300

    сонар доведен до рабочего состояния, warnings устранены.

--- src/board/omnibus/apps/granum/sonar/sonar_main.c
+++ src/board/omnibus/apps/granum/sonar/sonar_main.c
@@ -2,15 +2,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <nuttx/sensors/gy_us42.h>
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
 #else
-int blinky_main(int argc, char *argv[])
+int sonar_main(int argc, char *argv[])
 #endif
 {
-  printf("hello world, this is blinky app!\n");
-  int fd = open("/dev/range0", O_RDWR);
+  printf("hello world, this is sonar app!\n");
+  int fd = open("/dev/range0", O_RDONLY);
   if (fd < 0)
   {
       perror("cant open sonar device");
@@ -18,7 +19,15 @@ int blinky_main(int argc, char *argv[])
   }
 
   uint16_t val;
-  int rc = read(fd, &val, 2);
-  printf("rc: %d,    val: %d", rc, val);
+  unsigned long arg;
+  int rc1, rc2;
+  int cmd = GY_US42_IOCTL_CMD_MEASURE;
+  while(1) {
+	  rc1 = ioctl(fd, cmd, arg);
+	  usleep(500000);
+	  rc2 = read(fd, &val, 2);
+	  printf("rc1: %d, rc2 %d, val: %d, 0x%04X\n", rc1, rc2, val, val);
+	  sleep(1);
+  }
   return 0;
 }
