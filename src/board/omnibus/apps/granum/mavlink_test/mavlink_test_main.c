d25b936fcc1d05ccd960e77cf1deb853e844e3a0
Kirs67 <kirilel200267@gmail.com>
2018-04-25 18:45:16 +0300

    Борт отправляет данные IMU

---
+++ src/board/omnibus/apps/granum/mavlink_test/mavlink_test_main.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <nuttx/sensors/mpu6000.h>
+#include <nuttx/wireless/nrf24l01.h>
+#include <mavlink/granum/mavlink.h>
+
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int mavlink_test_main(int argc, char *argv[])
+#endif
+{
+	printf("This is MAVLink test app!\n");
+	int mpu_fd = open("/dev/mpu0", O_RDONLY | O_NONBLOCK);
+	if (mpu_fd < 0)
+	{
+		perror("Can't open /dev/mpu0");
+		return 1;
+	}
+
+	int nrf_fd = open("/dev/nrf24l01", O_RDWR | O_NONBLOCK);
+	if (nrf_fd < 0)
+	{
+	  perror("Can't open /dev/nrf24l01");
+	  return 1;
+	}
+
+	ioctl(mpu_fd, MPU6000_CMD_SET_CONVERT, true);
+
+	uint32_t tmp = 2489;
+	ioctl(nrf_fd, WLIOC_SETRADIOFREQ, (long unsigned int)&tmp);
+
+	tmp = 5;
+	ioctl(nrf_fd, NRF24L01IOC_SETADDRWIDTH, (long unsigned int)&tmp);
+
+	uint8_t addr[] = {0xBB, 0xBB, 0xBB, 0xBB, 0xBB};
+	ioctl(nrf_fd, NRF24L01IOC_SETTXADDR, (long unsigned int)addr);
+
+	tmp = 0;
+	ioctl(nrf_fd, WLIOC_SETTXPOWER, (long unsigned int)&tmp);
+
+	nrf24l01_retrcfg_t retrcfg = {.delay = DELAY_4000us, .count = 15};
+	ioctl(nrf_fd, NRF24L01IOC_SETRETRCFG, &retrcfg);
+
+	nrf24l01_pipecfg_t pipe0 = {.en_aa = true,\
+							  .payload_length = NRF24L01_DYN_LENGTH};
+	pipe0.rx_addr[0] = 0xAA;
+	pipe0.rx_addr[1] = 0xAA;
+	pipe0.rx_addr[2] = 0xAA;
+	pipe0.rx_addr[3] = 0xAA;
+	pipe0.rx_addr[4] = 0xAA;
+
+	nrf24l01_pipecfg_t pipeDummy = {.en_aa = true,\
+							  .payload_length = NRF24L01_DYN_LENGTH};
+	pipeDummy.rx_addr[0] = 0xAA;
+	pipeDummy.rx_addr[1] = 0xAA;
+	pipeDummy.rx_addr[2] = 0xAA;
+	pipeDummy.rx_addr[3] = 0xAA;
+	pipeDummy.rx_addr[4] = 0xAA;
+
+	nrf24l01_pipecfg_t * pipes[] = {&pipe0, &pipeDummy, &pipeDummy, &pipeDummy, &pipeDummy, &pipeDummy};
+
+	ioctl(nrf_fd, NRF24L01IOC_SETPIPESCFG, (long unsigned int)pipes);
+
+	uint8_t pipecfg = 0x01;
+	ioctl(nrf_fd, NRF24L01IOC_SETPIPESENABLED, (long unsigned int)&pipecfg);
+
+	nrf24l01_datarate_t datarate = RATE_1Mbps;
+	ioctl(nrf_fd, NRF24L01IOC_SETDATARATE, (long unsigned int)&datarate);
+
+	nrf24l01_crcmode_t crcmode = CRC_2BYTE;
+	ioctl(nrf_fd, NRF24L01IOC_SETCRCMODE, (long unsigned int)&crcmode);
+
+	nrf24l01_state_t state = ST_STANDBY;
+	ioctl(nrf_fd, NRF24L01IOC_SETSTATE, &state);
+
+	mpu6000_record_t record;
+	struct file * filep;
+
+	mavlink_scaled_imu_t imu_msg;
+	mavlink_message_t msg;
+
+	uint8_t buffer[1024];
+
+	while(1)
+	{
+		fs_getfilep(mpu_fd, &filep);
+		ssize_t isok = read(mpu_fd, &record, sizeof(record) );
+		printf("MPU6000: got %d bytes, error %d\n", isok >= 0 ? isok : 0, isok >=0 ? 0 : -get_errno());
+		if(isok < 0) continue;
+
+		imu_msg.time_boot_ms = record.time.tv_sec * 1000 + record.time.tv_nsec / 1000000;
+		imu_msg.xacc = (int)(record.acc.x * 1000);
+		imu_msg.yacc = (int)(record.acc.y * 1000);
+		imu_msg.zacc = (int)(record.acc.z * 1000);
+		imu_msg.xgyro = (int)(record.gyro.x * 1000 * M_PI / 180);
+		imu_msg.ygyro = (int)(record.gyro.y * 1000 * M_PI / 180);
+		imu_msg.zgyro = (int)(record.gyro.z * 1000 * M_PI / 180);
+		imu_msg.xmag = 0;
+		imu_msg.ymag = 0;
+		imu_msg.zmag = 0;
+
+		mavlink_msg_scaled_imu_encode(0, MAV_COMP_ID_IMU, &msg, &imu_msg);
+		uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+
+		isok = write(nrf_fd, buffer, len);
+		printf("NRF: wrote %d bytes, error %d\n", isok >= 0 ? isok : 0, isok >=0 ? 0 : -get_errno());
+
+		printf("_________________________________________________________________\n");
+		sleep(1);
+	}
+
+ 	return 0;
+}
--- src/ground/drainbow_gcs_nrf/src/main.cpp
+++ src/ground/drainbow_gcs_nrf/src/main.cpp
@@ -8,21 +8,78 @@
 #include <string>
 
 #include <RF24/RF24.h>
+#include <mavlink/granum/mavlink.h>
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 
+#include <boost/lexical_cast.hpp>
 
-int main()
+//TODO вынести в что-нибудь общее
+#define NRF_CHANNEL		89
+#define ADDR_ONBOARD	0xB0AFDB0AFD
+#define ADDR_GCS 		0x57A7104313
+
+int main(int argc, char ** argv)
 {
+	std::string addr_net;
+	std::string port;
+
+	if(argc >= 2) addr_net = argv[1];
+	else addr_net = "localhost";
+
+	if(argc >= 3) port = argv[2];
+	else port = "23728";
+
+	using namespace boost;
+	using udp = boost::asio::ip::udp;
+
 	RF24 radio1(22, BCM2835_SPI_CS0); //, BCM2835_SPI_SPEED_8MHZ);
 	RF24 radio2(24, BCM2835_SPI_CS1); //, BCM2835_SPI_SPEED_8MHZ);
 
 	radio1.begin();
 	radio2.begin();
 
-	radio1.openWritingPipe(0xAABBAABBAABBAABB);
-	radio2.openWritingPipe(0xCCDDCCDDCCDDCCDD);
+	radio1.setChannel(NRF_CHANNEL);
+	radio1.setAddressWidth(5);
+	radio1.setDataRate(RF24_1MBPS);
+	radio1.setCRCLength(RF24_CRC_16);
+	radio1.enableDynamicPayloads();
+	radio1.enableAckPayload();
+	//radio1.openWritingPipe(ADDR_ONBOARD);
+	//radio1.openReadingPipe(0, ADDR_ONBOARD);
+	radio1.openWritingPipe(0xAAAAAAAAAA);
+	radio1.openReadingPipe(1, 0xBBBBBBBBBB);
+	radio1.startListening();
 
-	radio1.printDetails();
-	radio2.printDetails();
+	uint8_t rx_pipeno;
+	uint8_t buffer[32];
+
+	try {
+		asio::io_service io;
+		udp::socket socket(io);
+		udp::resolver resolver(io);
+		udp::resolver::query query(udp::v4(), addr_net, port, udp::resolver::query::numeric_service);
+		udp::endpoint target_endpoint = *resolver.resolve(query);
+		socket.open(target_endpoint.protocol());
+
+		radio1.printDetails();
+
+		while(true) {
+			if( radio1.available(&rx_pipeno) ) {
+				size_t len = radio1.getDynamicPayloadSize();
+				radio1.read( buffer, len );
+				std::cout << "Got packet! " << len << std::endl;
+
+	            socket.send_to(asio::buffer(buffer, len), target_endpoint);
+			}
+		}
+	}
+
+	catch (std::exception & e) {
+		std::cout << "ERROR: " << e.what() << std::endl;
+	}
+
+	//radio2.openWritingPipe(0xCCDDCCDDCCDDCCDD);
 
 	std::cout << std::endl;
 	std::cout << std::endl;
