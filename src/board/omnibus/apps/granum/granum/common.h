dfbf257cb1bf3d94958e93604f0fe5955c3fd12a
Kirs67 <kirilel200267@gmail.com>
2018-06-28 21:38:36 +0300

    В конструкциях сделана FSM

--- src/board/omnibus/apps/granum/granum/common.h
+++ src/board/omnibus/apps/granum/granum/common.h
@@ -12,4 +12,37 @@
 
 #define DEBUG (void)//printf
 
+typedef enum {
+	GR_STATE_BOOTING = 0,
+	GR_STATE_READY,
+	GR_STATE_LOADED,
+	GR_STATE_FREE_FALL,
+	GR_STATE_AWAITING_CHUTE,
+	GR_STATE_PRE_LANDING,
+	GR_STATE_LANDED,
+} gr_state_t;
+
+extern gr_state_t state;
+
+typedef enum {
+	GR_EVENT_ACTIVATION,
+	GR_EVENT_EXIT,
+	GR_EVENT_250m,
+	GR_EVENT_CHUTE_GONE,
+	GR_EVENT_EMERGENCY_LANDING,
+	GR_EVENT_NEAR_GROUND,
+	GR_EVENT_LANDED
+} gr_event_t;
+
+typedef enum {
+	GR_SYSTEM_OMNIBUS = 1,
+	GR_SYSTEM_RASPBERRY,
+	GR_SYSTEM_GROUND,
+} gr_system_t;
+
+typedef enum {
+	GR_COMPONENT_OMNIBUS_MAIN = 0,
+	GR_COMPONENT_OMNIBUS_MADJWICK,
+} gr_component_omnibus_t;
+
 #endif /* APPS_GRANUM_GRANUM_COMMON_H_ */
--- src/board/omnibus/apps/granum/granum/granum_main.c
+++ src/board/omnibus/apps/granum/granum/granum_main.c
@@ -18,6 +18,8 @@
 #include <nuttx/sensors/bmp280.h>
 #include <nuttx/sensors/gy_us42.h>
 #include <nuttx/wireless/nrf24l01.h>
+#include <nuttx/sensors/tsl2561.h>
+
 #include <mavlink/granum/mavlink.h>
 #include "../../include/gpsutils/minmea.h"
 #include "madgwick/MadgwickAHRS.h"
@@ -29,7 +31,84 @@
 #include "threads/sensors_thread.h"
 #include "threads/interfaces_thread.h"
 
+//Global variables
 int nrf_fd, file_fd, fifo_fd, raspberry_fd;
+gr_state_t gr_state = GR_STATE_BOOTING;
+
+//Settings
+#define GR_CONFIG_LUX_THRESHOLD 400
+#define GR_CONFIG_DISTANCE_FREE_FALL_THRESHOLD 50
+
+
+#define MSG_BUF_SIZE 256
+
+static size_t _msg_carret = 0;
+static char _msg_buffer[MSG_BUF_SIZE];
+
+static bool _parseGPS(int fd, int cycles) {
+	while(cycles--) {
+		if(!_msg_carret) {
+			read(fd, _msg_buffer, 1);
+			if(_msg_buffer[0] == '$') _msg_carret++;
+		}
+		else {
+			read(fd, _msg_buffer + (_msg_carret++), 1);
+
+			if (_msg_carret >= MSG_BUF_SIZE)
+			{
+				// что-то не так
+				printf("Buffer overflow error!\n");
+				_msg_carret = 0;
+				break;
+			}
+
+			if('\r' == _msg_buffer[_msg_carret-2]
+			   &&	'\n' == _msg_buffer[_msg_carret-1]) {
+				_msg_buffer[_msg_carret] = '\x00';
+				_msg_carret = 0;
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+static void _translate_state(bool open_legs) {
+	switch(gr_state) {
+	case GR_STATE_BOOTING:
+	case GR_STATE_READY:
+		gr_state++;
+		break;
+
+	case GR_STATE_LOADED:
+		//FIXME starting spectro observation
+		gr_state++;
+		break;
+
+	case GR_STATE_FREE_FALL:
+		//FIXME open chute
+		gr_state++;
+		break;
+
+	case GR_STATE_AWAITING_CHUTE:
+		if(open_legs) {
+			//FIXME open legs
+		}
+
+		gr_state++;
+		break;
+
+	case GR_STATE_PRE_LANDING:
+		//FIXME detach chute
+		gr_state++;
+		break;
+
+	default:
+		break; //May be we should report?
+
+	}
+}
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
@@ -74,7 +153,7 @@ int granum_main(int argc, char *argv[])
 		return 1;
 	}
 
-	raspberry_fd = open("/dev/ttyS1", O_RDWR | O_NONBLOCK); //FIXME уточнить номер tty
+	raspberry_fd = open("/dev/ttyS1", O_RDWR | O_NONBLOCK);
 
 
 
@@ -146,14 +225,167 @@ int granum_main(int argc, char *argv[])
 	pthread_t madgwick_thread_id;
 	pthread_create(&madgwick_thread_id, 0, madgwick_thread, NULL);
 
-	pthread_t sensors_thread_id;
-	pthread_create(&sensors_thread_id, 0, sensors_thread, NULL);
-
 	pthread_t interfaces_thread_id;
 	pthread_create(&interfaces_thread_id, 0, interfaces_thread, NULL);
 
+//Opening files
+	int baro_fd = open("/dev/baro0", O_RDWR | O_NONBLOCK);
+	if (baro_fd < 0)
+	{
+		perror("Can't open /dev/baro0");
+		return 1;
+	}
+
+	int gps_fd = open("/dev/ttyS2", O_RDONLY);
+
+	if (gps_fd < 0)
+	{
+		perror("Can't open /dev/ttyS2");
+		return 1;
+	}
+
+	int sonar_fd = open("/dev/sonar0", O_RDONLY);
+	if (sonar_fd < 0)
+	{
+		perror("cant open sonar device");
+		return 1;
+	}
+
+	int tsl_fd = open("/dev/lumen0", O_RDONLY);
+	if (tsl_fd < 0)
+	{
+	  perror("cant open lumen device");
+	  return 1;
+	}
+
+	ioctl(tsl_fd, TSL2561_IOCTL_CMD_SETUP, NULL);
+
+
+//Settings
+//Настройки UART4 для GPS
+	struct termios termios;
+
+	ioctl(gps_fd, TCGETS, (unsigned long) &termios);
+
+	termios.c_cflag = 	(CS8 	 	//8 bits
+						| CREAD) 	//Enable read
+						& ~CSTOPB	//1 stop bit
+						& ~PARENB	//Disable parity check
+						& ~CRTSCTS; //No hardware flow control
+
+	ioctl(gps_fd, TCSETS, (unsigned long) &termios);
+
+
+
+//Messages and records
+	mavlink_scaled_pressure_t baro_msg;
+	bmp280_data_t result = {0, 0};
+
+	mavlink_sonar_t sonar_msg;
+	mavlink_luminosity_t luminosity_msg;
+
+	mavlink_hil_gps_t gps_msg;
+	gps_msg.cog = 0;
+	gps_msg.eph = 0;
+	gps_msg.epv = 0;
+	gps_msg.vd = 0;
+	gps_msg.ve = 0;
+	gps_msg.vel = 0;
+	gps_msg.vn = 0;
+	gps_msg.satellites_visible = 0xff;
+	struct timespec gps_time;
+	struct minmea_date gps_basedate = {
+			.year = 18,
+			.month = 5,
+			.day = 1,
+	};
+
+	mavlink_granum_message_t msg_msg;
+
+	mavlink_message_t msg;
+	uint8_t buffer[1024];
+
+	gr_state++;
+
+	struct timespec current_time;
+
 	while(true){
-		sleep(1);
+		clock_gettime(CLOCK_MONOTONIC, &current_time);
+		uint64_t time_boot_ms = current_time.tv_sec * 1000 + current_time.tv_nsec / 1000000;
+
+//TSL2561
+		read(tsl_fd, &luminosity_msg.luminosity, 2);
+		luminosity_msg.time_boot_ms = time_boot_ms;
+		mavlink_msg_luminosity_encode(GR_SYSTEM_OMNIBUS, GR_COMPONENT_OMNIBUS_MAIN, &msg, &sonar_msg);
+
+		int len = mavlink_msg_to_send_buffer(buffer, &msg);
+
+		ROUTE(ROUTE_WAY_TELEMETRY_COMMON, buffer, len)
+
+		if(gr_state == GR_STATE_LOADED && luminosity_msg.luminosity >= GR_CONFIG_LUX_THRESHOLD) _translate_state(NULL);
+
+//BMP280
+		ssize_t isok = read(baro_fd, &result, sizeof(result) );
+		DEBUG("BMP280: got %d bytes, error %d\n", isok >= 0 ? isok : 0, isok >=0 ? 0 : -get_errno());
+
+		clock_gettime(CLOCK_MONOTONIC, &current_time);
+		baro_msg.time_boot_ms = current_time.tv_sec * 1000 + current_time.tv_nsec / 1000000;
+		baro_msg.press_abs = result.pressure / 100.0;				//pressure in hPa
+		baro_msg.temperature = result.temperature * 100.0;		//temperature in 0.01 degC
+
+		mavlink_msg_scaled_pressure_encode(0, MAV_COMP_ID_PERIPHERAL, &msg, &baro_msg);
+		uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+
+		ROUTE(ROUTE_WAY_TELEMETRY_COMMON, buffer, len)
+
+		if(gr_state == GR_STATE_FREE_FALL || gr_state == GR_STATE_AWAITING_CHUTE) {
+			//TODO logic
+		}
+
+//SONAR
+		read(sonar_fd, &sonar_msg.distance, 2);
+		ioctl(sonar_fd, GY_US42_IOCTL_CMD_MEASURE, (unsigned int)NULL);
+		sonar_msg.time_boot_ms = time_boot_ms;
+		mavlink_msg_sonar_encode(GR_SYSTEM_OMNIBUS, GR_COMPONENT_OMNIBUS_MAIN, &msg, &sonar_msg);
+
+		len = mavlink_msg_to_send_buffer(buffer, &msg);
+
+		ROUTE(ROUTE_WAY_TELEMETRY_COMMON, buffer, len)
+
+		if(gr_state == GR_STATE_LOADED && sonar_msg.distance > GR_CONFIG_DISTANCE_FREE_FALL_THRESHOLD) _translate_state(NULL);
+
+		if(gr_state == GR_STATE_PRE_LANDING) {
+
+		}
+
+//GPS
+		if( _parseGPS(gps_fd, 100) ) {
+			// накопили, теперь разбираем
+			if (!minmea_check(_msg_buffer, false))
+				continue;
+
+			struct minmea_sentence_gga frame;
+			if (!minmea_parse_gga(&frame, _msg_buffer))
+				continue; // опс, что-то пошло не так
+
+			if (frame.fix_quality == 0)
+				continue;
+
+			minmea_gettime(&gps_time, &gps_basedate, &frame.time);
+
+			gps_msg.time_usec = gps_time.tv_sec * 1000000.0 + gps_time.tv_nsec / 1000.0;
+			gps_msg.lon = (int32_t)(minmea_tocoord(&frame.longitude) * 10000000);
+			gps_msg.lat = (int32_t)(minmea_tocoord(&frame.latitude) * 10000000);
+			gps_msg.alt = (int32_t)(minmea_tofloat(&frame.altitude) * 1000);
+			gps_msg.fix_type = frame.fix_quality;
+
+			mavlink_msg_hil_gps_encode(0, MAV_COMP_ID_PERIPHERAL, &msg, &gps_msg);
+			len = mavlink_msg_to_send_buffer(buffer, &msg);
+
+			ROUTE(ROUTE_WAY_TELEMETRY_COMMON, buffer, len)
+		}
+
+		usleep(100000);
 	}
 
 	printf("Something not good happened, and so, we are leaving main thread, brothers!\n");
